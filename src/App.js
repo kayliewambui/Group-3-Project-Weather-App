2393ac2bfea4e9c5cfc6abea21c721936a7ae2f3
Victor kamau <Victor Wangari>
2024-05-08 15:16:40 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import ProfileInputs from './components/Profile';
 
 function App() {
   return (
-    <div>
-      <div className='shadow-sm p-3 mb-2 bg-body-primary header'>
-        <h3>RESUME BUILDER</h3>
-      </div>
-    <ProfileInputs />
-    </div>
+    <></>
   );
 }
 
