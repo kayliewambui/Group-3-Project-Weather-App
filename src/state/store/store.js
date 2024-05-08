cb4c41331cbaf0d701fa70b1f85e6037dafc63a5
noel nyakundi <noel.nyakundi@student.moringaschool.com>
2024-05-09 00:09:39 +0300

    storedone

--- src/state/store/store.js
+++ src/state/store/store.js
@@ -2,3 +2,6 @@ import { applyMiddleware, compose, createStore } from "redux";
 import rootreducer from "../reducer";
 import thunk from "redux-thunk"
 
+const store=createStore(rootreducer,compose(applyMiddleware(thunk)))
+
+export default store;
