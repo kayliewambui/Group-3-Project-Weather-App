226c32af94f36d0a1353e3abf867766326150695
noel nyakundi <noel.nyakundi@student.moringaschool.com>
2024-05-13 22:15:05 +0300

    fixed basename

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-
 import './App.css';
 import Detail from './components/detail_filling_page/detail';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-
-
 import Aboutus from './components/about_us_page/aboutus';
-
 import Previewpage from './components/preview_page/preview';
 import Mytemplates from './components/homepage/my_templates';
 
-
-
 function App() {
-
-
   return (
-
-    <BrowserRouter><Routes>
-      <Route path='/' element={<Mytemplates />}></Route>
-      <Route path='det' element={<Detail/>}></Route>
-      <Route path="prev" element={<Previewpage/>}></Route>
-    
-      <Route path='/AboutUs'  element={<Aboutus/>}></Route>
-      
-    </Routes>
-
+    <BrowserRouter basename="/Group-3-Resume-Generator-App">
+      <Routes>
+        <Route path='/' element={<Mytemplates />} />
+        <Route path='det' element={<Detail />} />
+        <Route path="prev" element={<Previewpage />} />
+        <Route path='/AboutUs' element={<Aboutus />} />
+      </Routes>
     </BrowserRouter>
-
   );
 }
 
